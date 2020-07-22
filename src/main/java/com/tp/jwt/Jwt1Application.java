59c9d08e3ef8f3dc6eec15f1fd79b489698458ed
Ouiam <56403225+ouiam-dot@users.noreply.github.com>
2020-07-22 12:18:38 +0200

    Update Jwt1Application.java

--- src/main/java/com/tp/jwt/Jwt1Application.java
+++ src/main/java/com/tp/jwt/Jwt1Application.java
@@ -3,6 +3,7 @@ package com.tp.jwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Jwt1Application {
 
